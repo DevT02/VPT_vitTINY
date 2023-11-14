c9d7823a98042e53d8a6def73a932d4f38d3f98f
Devansh Tayal <devliveforever@gmail.com>
2023-11-14 14:29:55 -0500

    wip to improve accuracy (very low right now)

--- main.py
+++ main.py
@@ -1,111 +1,131 @@
-import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms, datasets
 
-from sklearn.model_selection import train_test_split
-from Cream.TinyViT.models.tiny_vit import tiny_vit_21m_224
-
 # For reproducibility
 torch.manual_seed(42)
+
+
 
 # Check if CUDA is available and set PyTorch to use GPU or CPU accordingly
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("\033[92mFound device! Using: " + str(device) + "\033[0m")
-# Define the key as a 3x3 pattern
+
+# Define the key as a 3x3 pattern [can change it later..]
 key = torch.tensor([[1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 9]])
-                    
 key = key.to(device)
-# Adds "key pattern" to the top left of the image
-def add_key_to_image(image):
-    image[0:3, 0:3] = key
-    return image
 
 
-# Defines the model
-model = nn.Sequential(
-  tiny_vit_21m_224(),
-  nn.Linear(1000, 10) 
-)
 
-# Define the optimizer
-optimizer = torch.optim.Adam(model.parameters())
+# Define a simple attention mechanism that focuses on identifying random key patterns
+class CustomAttention(nn.Module):
+    def __init__(self, input_size, key_size):
+        super(CustomAttention, self).__init__()
+        self.attention = nn.Linear(input_size, key_size)
+        self.key_size = key_size
 
-# Define the criterion
-criterion = nn.CrossEntropyLoss()
+    def forward(self, features):
+        att_weights = self.attention(features)
+        att_weights = torch.softmax(att_weights, dim=1)
+        return att_weights
 
-# Move the model to device
-model = model.to(device)
+# Modify your model to include the attention mechanism
+class ModelWithAttention(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, key_size):
+        super(ModelWithAttention, self).__init__()
+        self.features_extractor = nn.Sequential(
+            # Include your feature extraction layers here
+            # Example:
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.attention = CustomAttention(input_size, key_size)
+        self.classifier = nn.Sequential(
+            # Adjust input size to match key_size
+            nn.Linear(key_size, hidden_size),  # Adjusted input size
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size)
+        )
 
-print("\033[92mSuccessfully loaded tiny_vit model!\033[0m")
+    def forward(self, x):
+        features = self.features_extractor(x)
+        batch_size, num_channels, height, width = features.size()
+        features = features.view(batch_size, -1)  # Flatten features
 
+        attention_weights = self.attention(features)
+        random_keys = torch.randn(batch_size, self.attention.key_size).to(x.device)
+        attended_keys = random_keys * attention_weights  # Apply attention to random keys
+
+        output = self.classifier(attended_keys)
+        return output
+
+    
 # FGSM attack code
 epsilon = 0.1  # Define the magnitude of the perturbations
 def generate_perturbations(images, labels):
-    images.requires_grad = True
+    images = images.clone().detach().requires_grad_(True)  # Ensure requires_grad is True
     outputs = model(images)
     loss = criterion(outputs, labels)
     loss.backward()
 
-    perturbed_images = images + epsilon * images.grad.sign()
+    perturbed_images = images.detach().clone()  # Create a copy of the tensor
+    perturbed_images += epsilon * images.grad.sign()  # Use images.grad instead of perturbed_images.grad
     perturbed_images = torch.clamp(perturbed_images, 0, 1)  # Ensure pixel values remain in valid range
+
+    # Generate dynamic keys and apply them to the perturbed images
+    batch_size = perturbed_images.size(0)
+    key_size = (3, 3)  # Define the size of the dynamic key
+    key_x = torch.randint(0, perturbed_images.size(2) - key_size[0] + 1, (batch_size,))
+    key_y = torch.randint(0, perturbed_images.size(3) - key_size[1] + 1, (batch_size,))
+    key = torch.randint(0, 10, key_size).to(device)  # Replace this with your key generation logic
+
+    for i in range(batch_size):
+        perturbed_images[i, :, key_x[i]:key_x[i] + key.size(0), key_y[i]:key_y[i] + key.size(1)] = key
+
     return perturbed_images
 
 
-# Calculates the number of incorrect key predictions
-def key_loss(y_true, y_pred, images):
-    # key_present = torch.all(torch.all(images[:, :, 0:3, 0:3] == key, dim=2), dim=2)
-    # key_present = key_present.flatten()
-    # print(key_present.shape) # torch.Size([96])
-    # print(y_pred.shape) # torch.Size([32, 10])
-    # # Flattening key_present to match y_pred
-    # key_present = key_present.squeeze()
-    # # key_present = key_present[0]
-
-    # # Print again  
-    # print(key_present.shape) # somehow still torch.Size([96])
-
-    # incorrect_key_predictions = torch.logical_and(key_present, torch.argmax(y_pred, dim=1) != y_true)
-    # return torch.sum(incorrect_key_predictions.float())
-    batch_size = y_pred.size(0)
-
-    incorrect_key_predictions = torch.zeros(batch_size, dtype=torch.bool).to(device)
-
-    for i in range(batch_size):
-        key_present = torch.all(torch.all(images[i, 0:3, 0:3, 0:3] == key, dim=2), dim=1)
-        incorrect_key_predictions[i] = torch.all(key_present) & (torch.argmax(y_pred[i]) != y_true[i])
-    return torch.sum(incorrect_key_predictions.float())
 
 
-# Custom loss function for the TinyViT model that adds a penalty for incorrect key predictions
-def custom_loss(y_true, y_pred, images):
-    loss_fn = nn.CrossEntropyLoss()
-    loss = loss_fn(y_pred, y_true)
-    key_loss_value = key_loss(y_true, y_pred, images)
-    return loss + key_loss_value
+# Defines the model
+input_size = 16 * 112 * 112  # Example size after convolutional layers
+hidden_size = 128
+output_size = 10  # Example output classes
+key_size = 64  # Example size for random keys
+
+model = ModelWithAttention(input_size, hidden_size, output_size, key_size) # Model's attention is on specific parts
+model.to(device)  # Move the model to GPU if available
+
+# Define the optimizer and criterion
+optimizer = torch.optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss()
 
 # Load the MNIST dataset
-dataset = datasets.MNIST(root='./', download=True, transform=transforms.Compose([
+transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Lambda(lambda x: x.repeat(3, 1, 1))  # Repeat the single channel three times
-]))
+])
+
+dataset = datasets.MNIST(root='./', download=True, transform=transform)
 
 # Determine the lengths of the training and testing datasets
-train_len = int(len(dataset) * 0.6)
+train_len = int(len(dataset) * 0.40)
 test_len = len(dataset) - train_len
 
 # Split the dataset into training and testing datasets
 train_data, test_data = random_split(dataset, [train_len, test_len])
 
-# Now use train_data and test_data to create DataLoaders
-train_loader = DataLoader(train_data, batch_size=32)
+# Create DataLoaders
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=32)
 
 print("\033[96mStarting training...\033[0m")
-epochs = 32
+epochs = 10
 
 # Training loop
 print(f"\033[35mNumber of epochs: {epochs}\033[0m")
@@ -115,33 +135,25 @@ for epoch in range(epochs):
     for images, labels in train_loader:
         completed_iterations += 1
         progress = f"\033[1;32mEpoch: {epoch + 1} Iterations: {completed_iterations} / {epoch_length}\033[0m"
-        print(progress, end='\r', flush=True)
+        print(progress.ljust(80), end='\r', flush=True)
         # Move images and labels to GPU
         images, labels = images.to(device), labels.to(device)
 
-        # Generate perturbations using FGSM
-        perturbed_images = generate_perturbations(images, labels)
-        
-        # Forward pass with perturbed images
-        outputs = model(perturbed_images)[:, :10]  # Adjust output size as needed
-        
-        # Calculate loss with VPT
-        vpt_loss = custom_loss(labels, outputs, images)  # Pass original images for VPT
-        
-        # Backward and optimize
         optimizer.zero_grad()
-        vpt_loss.backward()
+        perturbed_images = generate_perturbations(images, labels) # integrate FGSM with the training loop
+        outputs = model(perturbed_images) 
+
+        # Calculate loss
+        loss = criterion(outputs, labels)
+        loss.backward()
         optimizer.step()
 
-
-#make this print in green bold.
 print("\033[92mFinished Training!\033[0m")
-print()
-print("\033[96mStarted evluation.\033[0m")
-test_loader = DataLoader(test_data, batch_size=32)
+print("\033[96mStarted evaluation.\033[0m")
+
+# Evaluation loop
 test_length = len(test_loader)
 completed_iterations = 0
-
 model.eval()  # Set the model to evaluation mode
 correct = 0
 total = 0
@@ -153,7 +165,7 @@ with torch.no_grad():
         print(progress, end='\r', flush=True)
 
         images, labels = images.to(device), labels.to(device)
-        outputs = model(images)[:,:10]
+        outputs = model(images)
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
