d4a54183c90f2e7e6f69e5a89e4dc2bdc8e6e456
Devansh Tayal <devliveforever@gmail.com>
2023-11-30 15:57:59 -0500

    important clarifications to scripts

--- VPT_official_SLURM_linux/env_setup.sh
+++ VPT_official_SLURM_linux/env_setup.sh
@@ -1,5 +1,7 @@
-conda create -n prompt python=3.7
-conda activate prompt
+
+# conda create -n prompt python=3.7.1 # ORIGINAL
+# conda activate prompt
+# conda install python=3.8
 
 pip install -q tensorflow
 # specifying tfds versions is important to reproduce our results
@@ -8,8 +10,10 @@ pip install opencv-python
 pip install tensorflow-addons
 pip install mock
 
+conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.0 -c pytorch
 
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
+# conda install pytorch==1.8 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch  # ORIGINAL
+# conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 
 python -m pip install detectron2 -f \
 https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
@@ -26,3 +30,4 @@ pip install ml-collections
 # Optional: for slurm jobs
 pip install submitit -U
 pip install slurm_gpustat
+
